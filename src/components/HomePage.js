87978ab98087a94a8642d742b8229ce1edd71d68
Michał R. Szmigiel <michal.r.szmigiel@gmail.com>
2020-01-14 11:36:44 +0100

    added components

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="jumbotron">
+      <h1>Course Administration</h1>
+      <p>React, Flux and React Router for ultra-responsive web apps.</p>
+      <a href="/about">About</a>
+    </div>
+  );
+};
+
+export default HomePage;
