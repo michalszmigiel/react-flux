e5e7e0d034152fb8723b678c7382b86ae5cf1c20
Michał R. Szmigiel <michal.r.szmigiel@gmail.com>
2020-01-14 18:49:07 +0100

    added form

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,20 @@ import CoursesPage from "./CoursesPage";
 import { Switch, Route, Redirect } from "react-router-dom";
 import FourOhFour from "./FourOhFour";
 import ManageCoursePage from "./ManageCoursePage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div className="container-fluid">
+      <ToastContainer autoClose={3000} hideProgressBar />
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course/" component={ManageCoursePage} />
         <Redirect from="/about-me" to="about" />
         <Route component={FourOhFour} />
       </Switch>
